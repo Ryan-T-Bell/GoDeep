d57eb4e15b8cb33778268f720ffe414119f6d234
Ryan-T-Bell <ryan.t.bell92@gmail.com>
2023-05-24 22:19:12 -0400

    multi os complete

--- server/forge/forge.go
+++ server/forge/forge.go
@@ -7,13 +7,18 @@ import (
 )
 
 func Generate() {
+	commands := [3]*exec.Cmd{
+    	exec.Command("env", "GOOS=linux", "GOARCH=amd64", "go", "build", "-o", "linux", "godeep/implant"),
+		exec.Command("env", "GOOS=windows", "GOARCH=amd64", "go", "build", "-o", "windows.exe", "godeep/implant"),
+		exec.Command("env", "GOOS=darwin", "GOARCH=amd64", "go", "build", "-o", "macos", "godeep/implant")}
+	
+	for _, cmd := range commands {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 
-    cmd := exec.Command("go", "build", "-o", "test.exe", "godeep/implant")
-    cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println(err)
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
